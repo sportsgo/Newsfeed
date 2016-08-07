187c954ef735c919b7a20633439b54cdc95e4a77
hpsilva <heliosilva35@hotmail.com>
2016-08-07 11:09:29 +0100

    first commit

---
+++ Static/app.js
@@ -0,0 +1,25 @@
+var main = function(){
+
+	// Parse JSON Data
+	var articles = $.getJSON( "https://raw.githubusercontent.com/sportsgo/sportsgo.github.io/master/js/data.json",
+		function(data) {
+        	console.log(data)
+        	for(object in data){
+        		var obj = data[object]
+        		console.log( obj )
+        		$("#articles").append('<div class="article">')
+        		$("div:last").append('<a href="' + obj.eventUrl + '" target="_blank"> <img src="' + obj.imageUrl + '"></a>')
+        		$("div:last").append('<p class="text">' + obj.description + '</p>')
+        		$("div:last").append('<hr>')
+
+	       		$("div:last").prepend('<div class="title">')	
+        		$("div:last").prepend('<a href="' + obj.eventUrl + '" target="_blank"> <span>' + obj.eventTitle + '</span></a> ')
+
+        	}
+	});
+
+	// Update Articles
+
+}
+
+$(document).ready(main)
